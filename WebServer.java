5824b6b5b827d9df76abe5c434bc9411a49be6e7
josh <rainiser@icloud.com>
2023-02-19 14:31:21 -0800

    Hello world test

--- WebServer.java
+++ WebServer.java
@@ -2,5 +2,7 @@ public class WebServer {
   public static void main(String[] args) {
     // This file will be compiled by script and must be at
     // the root of your project directory
+
+    System.out.println("Hello WORLD");
   }
 }
