d347944acc244a320ebba44b3c542c0f8a1065d2
josh <rainiser@icloud.com>
2023-02-19 16:12:47 -0800

    reader reading

--- reader/ConfReader.java
+++ reader/ConfReader.java
@@ -7,10 +7,22 @@ public class ConfReader extends Reader {
     public ConfReader(String path) {
         try {
             this.reader = new BufferedReader(new FileReader(path));
-            System.out.println("Success");
-        } catch (FileNotFoundException e) {
+            String line = reader.readLine();
+
+            while(reader.ready()){
+                if(line.trim().length() !=0 && !line.startsWith("#")){
+                    System.out.println(line);
+                }
+                line = reader.readLine();
+            }
+
+        } catch (IOException e) {
             System.out.println("Error initializing conf reader. File not found.");
             throw new RuntimeException(e);
         }
+    }
+
+    public static void main(String[] args) {
+        ConfReader test = new ConfReader("conf/httpd.conf");
     }
 }
