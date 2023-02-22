60b4e601548f346f1bc8957c2d23190d069b1755
josh <rainiser@icloud.com>
2023-02-21 23:23:11 -0800

    Able to accept requests but hangs.

--- WebServer.java
+++ WebServer.java
@@ -1,22 +1,39 @@
 import reader.ConfData;
 import reader.ConfReader;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Arrays;
 
 public class WebServer {
-  public static void main(String[] args) {
-    // This file will be compiled by script and must be at
-    // the root of your project directory
+    public static void main(String[] args) {
+        // This file will be compiled by script and must be at
+        // the root of your project directory
 
-    String confPath = "conf/httpd.conf";
-    ConfReader confReader = new ConfReader(confPath);
-    try {
-      ConfData config = confReader.read();
-      ServerSocket socket = new ServerSocket(config.getListen());
-      System.out.println("Server started...");
-    } catch (IOException e) {
-      throw new RuntimeException(e);
+        String confPath = "conf/httpd.conf";
+        ConfReader confReader = new ConfReader(confPath);
+        try {
+            ConfData config = confReader.read();
+            ServerSocket server = new ServerSocket(config.getListen());
+            System.out.println("Server started...");
+
+            while (true) {
+                Socket socket = server.accept();
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String line = in.readLine();
+                if (line != null) {
+                    System.out.println(Arrays.toString(line.split(" / ")));
+                }
+                line = in.readLine();
+                while (line != null) {
+                    System.out.println(line);
+                    line = in.readLine();
+                }
+                socket.close();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
-  }
 }
